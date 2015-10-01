34ab52c9870841dda79d1eb1dd635e7a8d49e28a
Murray Anderson-Clemence <me@murray.ac>
2015-10-02 01:18:08 +1000

    Update functions.php
    Worked it out thanks to this tutorial http://chriseggleston.com/ambiance-like-fullwidth-image-in-pages-and-posts-in-genesis/

--- functions.php
+++ functions.php
@@ -1,10 +1,29 @@
+//* Enqueue scripts for backstretch
+add_action( 'wp_enqueue_scripts', 'whitespace_front_page_enqueue_scripts' );
+function whitespace_front_page_enqueue_scripts() {
+	
+	$image = get_option( 'whitespace-home-image', sprintf( '%s/images/welcome.jpg', get_stylesheet_directory_uri() ) );
+	
+	//* Load scripts only if custom backstretch image is being used
+	if ( is_singular( array( 'post', 'page' ) ) && has_post_thumbnail() ) {
+
+		//* Enqueue Backstretch scripts
+		wp_enqueue_script( 'whitespace-backstretch', get_bloginfo( 'stylesheet_directory' ) . '/js/backstretch.js', array( 'jquery' ), '1.0.0' );
+		wp_enqueue_script( 'whitespace-backstretch-set', get_bloginfo('stylesheet_directory').'/js/backstretch-set.js' , array( 'jquery', 'whitespace-backstretch' ), '1.0.0' );
+
+		$image = array( 'src' => has_post_thumbnail() ? genesis_get_image( array( 'format' => 'url' ) ) : '' );
+		wp_localize_script( 'whitespace-backstretch-set', 'BackStretchImg', $image );
+	
+	}
+
+}
+
 //* Display featured image before single post title
 add_action( 'genesis_before_entry', 'featured_post_image', 8 );
 function featured_post_image() {
-  if ( ! is_singular( 'post' ) )  return;
-
-	the_post_thumbnail( 'welcome', array(
-		'before' => '<div class="welcome">',
-		'after'  => '</div>',
-	) );
+	if ( is_singular( 'post' ) || ( is_singular( 'page' ) && has_post_thumbnail() ) ) {
+		
+		echo '<div class="welcome"></div>';
+	
+	}
 }
